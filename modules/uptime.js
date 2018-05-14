a25233a6e1802e4a8f9c1f38d106c6d32e20a292
Anna Gajewska <gajewska.anna@o2.pl>
2018-05-14 23:51:35 +0200

    Change in uptime.js

--- modules/uptime.js
+++ modules/uptime.js
@@ -3,8 +3,8 @@ var os = require('os');
 function uptime() {
 	var uptime = os.uptime();
 	var hour = (uptime / 3600).toFixed(0);
-	var min = (uptime%3600/60).toFixed(0);
-	var sec = (uptime%60).toFixed(0);
+	var min = (uptime % 3600 / 60).toFixed(0);
+	var sec = (uptime % 60).toFixed(0);
 		if (uptime > 3600) {
 			console.log('Uptime:', hour, 'godz.', min, 'min.', sec, 'sek.');
 		} else if (uptime > 60) {
